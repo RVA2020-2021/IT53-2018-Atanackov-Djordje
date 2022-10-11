04cc12895347dea7b86bd1c33d5aa34a3f10ff5e
atanackov12 <105231693+atanackov12@users.noreply.github.com>
2022-10-11 18:16:57 +0200

    commit 4-klijentRestController-crud

--- Starter-project/src/main/java/rva/ctrls/KlijentRestController.java
+++ Starter-project/src/main/java/rva/ctrls/KlijentRestController.java
@@ -3,8 +3,14 @@ package rva.ctrls;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import rva.jpa.Klijent;
@@ -30,6 +36,33 @@ public class KlijentRestController {
 	@GetMapping("klijentIme/{ime}")
 	public Collection<Klijent> getKlijentByIme(@PathVariable("ime") String ime){
 		return klijentRepository.findByImeContainingIgnoreCase(ime);
+	}
+	
+	@PostMapping("klijent")
+	public ResponseEntity<Klijent> insertKlijent(@RequestBody Klijent klijent){
+		if(!klijentRepository.existsById(klijent.getId())) {
+			klijentRepository.save(klijent);
+			return new ResponseEntity<Klijent>(HttpStatus.OK);
+		}
+		return new ResponseEntity<Klijent>(HttpStatus.CONFLICT);
+	}
+	
+	@PutMapping("klijent")
+	public ResponseEntity<Klijent> updarteKlijent(@RequestBody Klijent klijent){
+		if(!klijentRepository.existsById(klijent.getId())) {
+			return new ResponseEntity<Klijent>(HttpStatus.NO_CONTENT);
+		}
+		klijentRepository.save(klijent);
+		return new ResponseEntity<Klijent>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("klijent/{id}")
+	public ResponseEntity<Klijent> deleteKlijent(@PathVariable("id") Integer id){
+		if(!klijentRepository.existsById(id)) {
+			return new ResponseEntity<Klijent>(HttpStatus.NO_CONTENT);
+		}
+		klijentRepository.deleteById(id);
+		return new ResponseEntity<Klijent>(HttpStatus.OK);
 	}
 
 }
