14e3d52426a617bf8ca7e729acb22134d9449ff8
atanackov12 <105231693+atanackov12@users.noreply.github.com>
2022-11-02 20:32:47 +0100

    backend complete

--- Starter-project/src/main/java/rva/ctrls/KreditRestController.java
+++ Starter-project/src/main/java/rva/ctrls/KreditRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import rva.jpa.Kredit;
 import rva.repository.KreditRepository;
 
+@CrossOrigin
 @RestController
 public class KreditRestController {
 	
@@ -68,8 +70,7 @@ public class KreditRestController {
 		if(id == -100) {
 			jdbcTemplate.execute(
 					"INSERT INTO \"kredit\"(\"id\", \"naziv\", \"opis\", \"oznaka\") "	
-					 + "VALUES (-100, 'Naziv Test', 'Opis Test', 'OzTest')"	
-		   );
+					 + "VALUES (-100, 'Naziv Test', 'Opis Test', 'Oznaka test')"  );
 		}
 		return new ResponseEntity<Kredit>(HttpStatus.OK);
 	}
